1310f85f0dd3ed9b1c4109bc2c4fe11c8fbf7cb9
Dawid Szymański <sq6emm@hamradio.pl>
2020-12-21 00:42:14 +0100

    add cron

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import logging, sys
+import logging, sys, time
 from fastapi import FastAPI
 from pyownet import protocol
 from gpiozero import OutputDevice
+from apscheduler.schedulers.background import BackgroundScheduler
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 app = FastAPI(
   title = "Heating API",
@@ -13,13 +14,19 @@ app = FastAPI(
   version = "beta"
 )
 
-@app.get("/heating", tags=["heating"])
-def get_heating():
-  sensorsdict = {}
-  sensorsdict['28.B22793050000'] = 'main.floor'
-  sensorsdict['28.858D94050000'] = 'main.wall'
+req_temp = 5
+
+sensorsdict = {}
+sensorsdict['28.858D94050000'] = 'main.floor'
+sensorsdict['28.B22793050000'] = 'main.wall'
+
+def getTempOfSensor(sname):
+  for i in readTemperature():
+    if i.get('name') == sname:
+      return float(i.get('temp'))
+
+def readTemperature():
   sensorList = list()
-  heatingList = list()
   host, port = "localhost", 4304
   proxy = protocol.proxy(host, port)
   pid = int(proxy.read('/system/process/pid'))
@@ -28,13 +35,27 @@ def get_heating():
     stype = proxy.read(sensor + '/type').decode()
     if stype == "DS18B20":
       temp = float(proxy.read(sensor + '/temperature'))
-      temp = "{0:.1f}".format(temp)
+      temp = "{0:.2f}".format(temp)
       sid = sensor.strip("/")
       if sid in sensorsdict:
         sname = sensorsdict[sid]
       else:
         sname = "unknown"
       sensorList.append({"id":sid, "name":sname, "temp":temp})
-  mainheater = OutputDevice(25)
-  heatingList.append({"id":"gpio25","name":"main", "status":mainheater.value})
+  return sensorList
+
+def job():
+  if getTempOfSensor("main.wall") < req_temp:
+    print("powinnismy zaczac grzanie")
+
+@app.get("/heating", tags=["heating"])
+def get_heating():
+  heatingList = list()
+  sensorList = readTemperature()
+#  mainheater = OutputDevice(25)
+#  heatingList.append({"id":"gpio25","name":"main", "status":mainheater.value})
   return { "sensors": sensorList, "heaters": heatingList }
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(job, 'interval', minutes=1)
+scheduler.start()
